ca0d6125fb54300ff6a7ce75e8558f41af010c90
galimovserg <sergey.galimov.97@mail.ru>
2017-12-19 19:14:50 +0300

    Изменил тест

--- calckwindow/src/TestWindow.java
+++ calckwindow/src/TestWindow.java
@@ -10,54 +10,56 @@ public class TestWindow {
 
 	@Test
 	public void test1() {
-		//fail("Not yet implemented");
-		//—оздаем проект, им€ "test", количество створок 1
-		ProjectPanel ProjectTest=new ProjectPanel("test",1);
+		//—оздаем проект, им€ "test1", количество створок 1
+		ProjectPanel ProjectTest=new ProjectPanel("test1",1);
 		//устанавливаем свойства окна
 		ProjectTest.leftpanel.comboBox1.setSelectedIndex(0);
-		ProjectTest.leftpanel.comboBox2.setSelectedIndex(1);
-		ProjectTest.leftpanel.comboBox3.setSelectedIndex(2);
-		ProjectTest.leftpanel.comboBox4.setSelectedIndex(2);
-		ProjectTest.leftpanel.comboBox5.setSelectedIndex(1);
-		ProjectTest.leftpanel.comboBox6.setSelectedIndex(2);
+		ProjectTest.leftpanel.comboBox2.setSelectedIndex(0);
+		ProjectTest.leftpanel.comboBox3.setSelectedIndex(0);
+		ProjectTest.leftpanel.comboBox4.setSelectedIndex(0);
+		ProjectTest.leftpanel.comboBox5.setSelectedIndex(0);
+		ProjectTest.leftpanel.comboBox6.setSelectedIndex(0);
 		//устанавливаем типы дл€ каждой створки
 		ProjectTest.rightpanel.panelw1.settype(Section.none);
-		ProjectTest.rightpanel.panelw2.settype(Section.right);
-		ProjectTest.rightpanel.panelw3.settype(Section.left);
+		ProjectTest.rightpanel.panelw2.settype(Section.none);
+		ProjectTest.rightpanel.panelw3.settype(Section.none);
 		ProjectTest.rightpanel.panelw4.settype(Section.none);
-		//мен€ем тип второй и третьей створки
-		ProjectTest.rightpanel.panelw2.nexttype();
-		ProjectTest.rightpanel.panelw3.nexttype();
+		
 		//устанавливаем количество створок 3
-		ProjectTest.rightpanel.setcount(3);
+		ProjectTest.rightpanel.setcount(1);
 		//задаем ширину окна
-		ProjectTest.rightpanel.textbox1.setText("1500");
+		ProjectTest.rightpanel.textbox1.setText("500");
+		//задаем высоту окна
+		ProjectTest.rightpanel.textbox2.setText("800");
 		//вычисл€ем и сравниваем полученные результаты
-		assertEquals(ProjectTest.calck(), "—тоимость: 22503,65 руб.");
+		
+		assertEquals(ProjectTest.calck(), "—тоимость: 3770,12 руб.");
 	}
 	@Test
 	public void test2(){
 		//создаем проект, им€ "test", количество створок 3
-		ProjectPanel ProjectTest=new ProjectPanel("test",3);
+		ProjectPanel ProjectTest=new ProjectPanel("test2",3);
 		//вычисл€ем и сравниваем полученные результаты
 		assertEquals(ProjectTest.calck(), "—тоимость: 15792,20 руб.");
-		
+		ProjectTest.leftpanel.comboBox6.setSelectedIndex(1);
+		//вычисл€ем и сравниваем полученные результаты
+		assertEquals(ProjectTest.calck(), "—тоимость: 18792,20 руб.");
 	}
 	@Test
 	public void test3(){
 		//создаем проект, им€ "test", количество створок 4
-		ProjectPanel ProjectTest=new ProjectPanel("test",4);
+		ProjectPanel ProjectTest=new ProjectPanel("test3",4);
 		//задаем ширину окна
-		ProjectTest.rightpanel.textbox1.setText("1900");
+		ProjectTest.rightpanel.textbox1.setText("4000");
 		//вычисл€ем и сравниваем полученные результаты
-		assertEquals(ProjectTest.calck(), "Ўирина окна должна быть не менее 2000");
+		assertEquals(ProjectTest.calck(), "Ўирина окна должна быть не более 3600");
 	}
 	@Test
 	public void test4(){
 		//создаем проект, им€ "test", количество створок 4
-		ProjectPanel ProjectTest=new ProjectPanel("test",4);
+		ProjectPanel ProjectTest=new ProjectPanel("test4",1);
 		//в текстовом поле ширины окна вводим данные неверного формата
-		ProjectTest.rightpanel.textbox1.setText("aaaa");
+		ProjectTest.rightpanel.textbox2.setText("оуоуоу");
 		//вычисл€ем и сравниваем полученные результаты
 		assertEquals(ProjectTest.calck(), "Ќеверный формат данных");
 	}
